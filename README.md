ca4b0250e26e49733c5d500a0e4bdc7a09099742
She <sheverwerft@gmail.com>
2023-11-30 14:42:46 +0100

    Fully working Add-All button when selling an item

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Adds additonal options to the bazaar sell tab in Fallen London
 
 - Add a button to when you are selling items that fills in the max amount of items you own of that type.
 
-- A button next to sell that just does whats listen above
+- A button next to sell that just does whats listed above
 
 - or maybe just a bunch of checkboxes and then sell all of the checked items
 
@@ -14,3 +14,28 @@ Adds additonal options to the bazaar sell tab in Fallen London
 ### resources i used to help me out
 https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
 https://www.w3schools.com/js/js_htmldom_nodes.asp
+
+```
+var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
+nativeInputValueSetter.call(input, 'react 16 value');
+
+var ev2 = new Event('input', { bubbles: true});
+input.dispatchEvent(ev2);
+
+
+// orrrrr
+
+function setReactvalue(inputElement, newValue) {
+  // These two lines do the difficult version of input.value = newValue; 
+  let hiddenInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
+  hiddenInputValueSetter.call(inputElement, newValue);
+
+  // These two lines just run the inputelement.onInput event listener
+  let fakeEvent = new Event('input', { bubbles: true });
+  inputElement.dispatchEvent(fakeEvent );
+}
+
+// Example usage
+setReactValue(document.getQuerySelector("input[type='number']", 20);
+
+```
--- sellall.js
+++ sellall.js
@@ -1,48 +1,57 @@
 const bazaarPage = "https://www.fallenlondon.com/bazaar";
-const checkerConfig = { subtree: true, childList: true };
+const checkerConfig = {
+    subtree: true,
+    childList: true
+};
 //make the fill all button
 const fillAllButton = document.createElement("button");
 //give it the classes fallen london uses for style
-fillAllButton.className ='button button--primary fillAllButton';
+fillAllButton.className = 'button button--primary fillAllButton';
 //very important. Add Type Button because without it it will sell your items. (Rat on a string, you fought the good fight.)
 fillAllButton.type = 'button';
 // go checkin for stuff when on bazaar page only
 const sellPopupChecker = new MutationObserver(function () {
     if (window.location.href == bazaarPage) {
         lookForSellPopup()
-        console.log('looking for sell popup');
-    }
-    else {
+    } else {
         return;
     }
-    
-    
+
+
 });
 
-function lookForSellPopup () {
+function lookForSellPopup() {
     //try and find the sell item popup
     let sellPopup = window.document.querySelector('.modal-dialog--after-open');
-    //check if the fillAll button has already been added otherwise it will add an infinit amount of them.
-    let lookForFillAllButton = sellPopup.querySelector('.fillAllButton');
-    //honestly. tryna fix this error. TODO
-    if (sellPopup == null) {
-        return;
+    //if the sellpopup has been found. do
+    if (sellPopup != null) {
+        //look for the fillAllButton 
+        let lookForFillAllButton = sellPopup.querySelector('.fillAllButton');
+        //check if the fillAll button has already been added otherwise it will add an infinit amount of them.
+        if (lookForFillAllButton == null) {
+            //find the control buttons
+            let sellPopupControls = sellPopup.querySelector('.exchange-ui__controls');
+            //find total items owned
+            let itemTotal = sellPopup.querySelector('.js-item-value.icon__value').innerText;
+            //insert the fillall button with itemTotal as its text
+            sellPopupControls.insertAdjacentElement('beforeend', fillAllButton);
+            fillAllButton.innerText = "+" + itemTotal;
+            let numberField = sellPopupControls.querySelector('input[type="number"]');
+            fillAllButton.addEventListener("click", function () {  
+                let setSellTotal = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set;
+                setSellTotal.call(numberField, itemTotal);
+                let fakeEvent = new Event('input', {bubbles: true});
+                numberField.dispatchEvent(fakeEvent);                
+         });
+            
+            
+        }
+
+
     }
     //if the button hasnt already been made, find total items owned. add it as text to the FillAll button.
-    //add the button to the page.
-    else if (lookForFillAllButton == null) {
-        let sellPopupControls = sellPopup.querySelector('.exchange-ui__controls');
-        let itemTotal = sellPopup.querySelector('.js-item-value.icon__value').innerText;
-        const fillAllButtonNode = document.createTextNode(itemTotal);
-        fillAllButton.appendChild(fillAllButtonNode);
-        sellPopupControls.insertAdjacentElement('beforeend', fillAllButton);
-        console.log(itemTotal);
-        sellPopup.style.color = 'red';
-        console.log(sellPopup);
-    } 
+
 };
-
-
 
 
 sellPopupChecker.observe(document, checkerConfig);
